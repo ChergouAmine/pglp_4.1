358d525f08124544b8c87d3cd5a4024043ad9f20
Utilisateur <user@debian.arcahe.ovh>
2020-03-30 18:56:50 +0200

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 
 public class Main {
 
@@ -13,6 +14,44 @@ public class Main {
 		
 		c.print();
 */
+	  
+	  Personnels p1 = new Personnel
+          .Builder("amine", "chergou", LocalDateTime.now())
+          .add_tel(0666666666)
+          .function("directeur")
+          .build();
+	  Personnels p2 = new Personnel
+          .Builder("aminee", "chergouu", LocalDateTime.now())
+          .add_tel(0666666666)
+          .function("pdg")
+          .build();
+	  Personnels p3 = new Personnel
+          .Builder("amineee", "chergouuu", LocalDateTime.now())
+          .add_tel(0666666666)
+          .function("chef d'équipe")
+          .build();
+	  Personnels p4 = new Personnel
+          .Builder("amineeee", "chergouuuu", LocalDateTime.now())
+          .add_tel(0666666666)
+          .function("programmeur")
+          .build();
+	  Personnels p5 = new Personnel
+          .Builder("amineeeee", "chergouuuuu", LocalDateTime.now())
+          .add_tel(0666666666)
+          .function("développeur")
+          .build();
+
+  
+	  CompositePersonnel g1 = new CompositePersonnel(1);
+	  g1.add(p1);
+	  g1.add(p2);
+	  g1.add(p3);
+	  CompositePersonnel g2 = new CompositePersonnel(2);
+	  g2.add(p4);
+	  g2.add(p5);
+	  CompositePersonnel g3 = new CompositePersonnel(3);
+	  g3.add(g1);
+	  g3.add(g2);
 	}
 
 }
